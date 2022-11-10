3659f26775e140c2e8d7afa6e5843b5830f446d9
TomSawyer2 <6272867@qq.com>
2022-11-10 23:33:46 +0800

    chore: test CICD

--- src/components/HeaderBar/index.tsx
+++ src/components/HeaderBar/index.tsx
@@ -6,7 +6,7 @@ const HeaderBar: React.FC = () => {
   return (
     <div className="navbar">
       <div className="navbar-box">
-        <span className="navbar-title">Python_QRCode WebUI</span>
+        <span className="navbar-title">Bee_Farming</span>
       </div>
     </div>
   );
